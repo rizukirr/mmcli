a43cf6d9cc0c46dc2cda0012e20e8216d7f90fdd
rizukirr <rizkirr.xyz@gmail.com>
2026-07-01 11:45:48 +0700

    feat: add constants module to replace config layer

---
+++ app/utils/constants.py
@@ -0,0 +1,6 @@
+"""Hardcoded defaults for the few values dependencies don't provide."""
+
+APP_VERSION = "0.1.0b1"
+
+# pytubefix/ffmpeg provide quality defaults; only playlist fan-out needs a constant.
+PLAYLIST_MAX_CONCURRENT = 3
